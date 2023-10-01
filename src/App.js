6a4e0a60457570c39369a8bdcb4359a738d73d63
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-10-01 19:11:18 +0330

    profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Pages from "./Pages/Pages";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App" >
+      <Pages />
     </div>
   );
 }
